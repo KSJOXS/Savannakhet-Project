6a2db5e6517ce8f6be3716a56e3d1706aace0d2b
LAPTOP-P2GSFH8L\ASUS <jo.xaysongkham99@gmail.com>
2026-04-06 16:53:34 +0700

    fix: update vite base path for github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,8 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 💡 เพิ่มบรรทัดนี้ (ใส่ชื่อ Repository ของคุณ)
+  base: '/Savannakhet-Project/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
